edd0a33f8c55c4540882287cfa71e7bf1966ca6f
Andy Wu <www.flamer1959@hotmail.com>
2025-04-30 22:05:25 -0700

    Project configs

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,15 +1,55 @@
-import { defineConfig } from '@playwright/test';
+import { PlaywrightTestConfig } from '@playwright/test';
+import { devices } from '@playwright/test';
 
-export default defineConfig({
+const { BASE_URL } = process.env;
+const baseURL = BASE_URL || 'https://shop.lululemon.com/en-ca/c/bags/n1rdci';
+
+const config: PlaywrightTestConfig = {
   testDir: './tests',
-  timeout: 30000,
+  timeout: 60000,
   expect: {
-    timeout: 5000,
+    timeout: 20000,
   },
-  reporter: 'html',
+  reporter: [
+    ['list'], 
+    ['html', {open: 'never', outputFolder: 'reports'}],
+    ['junit', {outputFile: 'reports/results.xml'}],
+  ],
   use: {
     headless: true,
     actionTimeout: 0,
-    trace: 'on-first-retry',
+    baseURL: baseURL ?? 'https://shop.lululemon.com/en-ca/c/bags/n1rdci',
+    trace: 'on',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    ignoreHTTPSErrors: true,
+    bypassCSP: true,
   },
-});
+
+  projects: [
+    {
+      name: 'Desktop Chrome',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+    {
+      name: 'Mobile Safari',
+      use: {
+        ...devices['iPhone 12'],
+        viewport: { width: 375, height: 812 },
+      },
+    },
+    {
+      name: 'Mobile Chrome',
+      use: {
+        ...devices['Pixel 5'],
+        viewport: { width: 412, height: 915 },
+      },
+    }
+  ],
+
+  outputDir: './artifacts',
+};
+
+export default config;
